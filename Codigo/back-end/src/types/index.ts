8772ad08ec79fee0aa826141e6caddb2c95807c3
Guicbdiniz <guicbdeniz@hotmail.com>
2020-09-05 15:27:34 -0300

    Adicionar interface de Trabalhador

---
+++ Codigo/back-end/src/types/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Trabalhor model's interface.
+ */
+interface Trabalhador {
+	cpf: Number
+	senha: String
+	nomeCompleto: String
+	nomeCompletoPai: String
+	nomeCompletoMae: String
+	numeroDeRG: Number
+	dataDeNascimento: String
+	localDeNascimento: String
+	estadoCivil: String
+	temFilhos: Boolean
+	telefoneDeContato: String
+	email: String
+	endereco: String
+	escolaridade: String
+	experienciasProfissionais: String[]
+	objetivoProfissional: String
+	resumoProfissional: String
+}
