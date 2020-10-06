bfa0c5ab470a891766d3cca277a94f1831e6ea90
Guicbdiniz <guicbdeniz@hotmail.com>
2020-10-06 20:47:25 -0300

    DB Checkers hot fix. The usuario table is created before the trabalhador table now.

--- Codigo/back-end/src/routers/TrabalhadorRouter.ts
+++ Codigo/back-end/src/routers/TrabalhadorRouter.ts
@@ -1,20 +1,20 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import {
-    create,
-    login,
-    getTrabalhador,
-    deleteTrabalhador,
-    getAllTrabalhadores,
+	create,
+	login,
+	getTrabalhador,
+	deleteTrabalhador,
+	getAllTrabalhadores
 } from '../controllers/TrabalhadorController'
-import {checkTrabalhador, checkUsuario} from '../middlewares/DatabaseCheckers'
-import {authenticateJWTToken} from '../middlewares/TokenAuth'
+import { checkTrabalhador, checkUsuario } from '../middlewares/DatabaseCheckers'
+import { authenticateJWTToken } from '../middlewares/TokenAuth'
 
 const router = Router()
 
 // Middleware to check DB tables.
 // Do not use them in production.
-router.use(checkTrabalhador)
 router.use(checkUsuario)
+router.use(checkTrabalhador)
 
 router.post('/register/', create)
 router.post('/login/', login)
