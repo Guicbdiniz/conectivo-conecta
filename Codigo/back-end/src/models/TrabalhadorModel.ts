16dfc603eb30578ee955ae70197c9b1f935fec41
reconectar <arthurgbranco2@gmail.com>
2020-10-06 17:14:24 -0300

    added delete usuario to delete trabalhador

--- Codigo/back-end/src/models/TrabalhadorModel.ts
+++ Codigo/back-end/src/models/TrabalhadorModel.ts
@@ -93,7 +93,14 @@ export function insertUsuario(usuario: Usuario): Promise<Usuario> {
 export function removeTrabalhador(email: String): Promise<String> {
     return new Promise(function (resolve, reject) {
         const queryString =
-            'DELETE FROM `trabalhador` ' + "WHERE `email` = '" + email + "';"
+            'DELETE FROM `trabalhador` ' +
+            "WHERE `email` = '" +
+            email +
+            "';" +
+            'DELETE FROM `usuario` ' +
+            "WHERE `email` = '" +
+            email +
+            "';"
 
         sql.query(queryString, function (err, res) {
             if (err) {
