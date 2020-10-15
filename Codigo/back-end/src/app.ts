ed3cb0778b1b92ac2bdd279d665db78936071e78
Guicbdiniz <guicbdeniz@hotmail.com>
2020-10-14 22:41:36 -0300

    Middleware de checagem de banco atualizado

--- Codigo/back-end/src/app.ts
+++ Codigo/back-end/src/app.ts
@@ -8,12 +8,14 @@ import bodyParser from 'body-parser'
 
 import TrabalhadorRouter from './routers/TrabalhadorRouter'
 import HelloRouter from './routers/HelloRouter'
+import { checkDatabase } from './middlewares/DatabaseCheckers'
 
 const app = express()
 
 // Global Middlewares
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(checkDatabase)
 
 // Routes
 app.use('/hello', HelloRouter)
